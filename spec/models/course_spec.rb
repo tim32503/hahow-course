8f37f9a52db8355503319a858c79cc49ff744015
Tim Hsueh <timhsueh@curihaosity.xyz>
2024-07-04 16:34:05 +0800

    update: 新增 Course Model 的測試

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,5 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Course, type: :model do
   context 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:lecturer_name) }
   end
 end
